eaaf2ff4364698b3de0229972299dd075af757fb
genuinemax <pradipone27@gmail.com>
2026-09-08 20:25:18 +0545

    Enable isolated mobile GUI layer

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './stockSearch.js'
+import './mobile-gui.js'
 import App from './App.jsx'
 
 if ('serviceWorker' in navigator && import.meta.env.PROD) {
