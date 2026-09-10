44916a74cc411c12015f1ad1a1bc1acd96844e2a
genuinemax <pradipone27@gmail.com>
2026-09-10 11:25:43 +0545

    chore: invalidate old app shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'genuine-fix-shell-v2';
+const CACHE_NAME = 'genuine-fix-shell-v3';
 const APP_SHELL = ['/', '/index.html', '/favicon.svg', '/manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
